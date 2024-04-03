3d48a68e7d34df29cf618273bafb1375b01c0653
kotonchik <anton.mut05@gmail.com>
2024-04-03 16:23:06 +0200

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import * as process from "process";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 async function start() {
   const PORT = process.env.PORT || 4000
   const app = await NestFactory.create(AppModule)
+
+  const config = new DocumentBuilder()
+    .setTitle('Project example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('Project')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('Swagger', app, document);
+
 
   await app.listen(PORT, () => console.log(`Server started on port - ${PORT}`))
 
